20e6a2736327efd220647f1545736249da615edf
Sanskar Gupta <sanskar20021@gmail.com>
2025-03-31 01:33:59 +0530

    All models done

---
+++ app.js
@@ -0,0 +1,10 @@
+import { Express } from "express";
+
+const app = express();
+
+//router imports
+import healthCheckRouter from "./routes/healthcheck.route.js"
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+export default app;
